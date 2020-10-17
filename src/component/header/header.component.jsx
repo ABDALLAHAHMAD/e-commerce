eb6ef47cd09a6ab6d8e31fca988a88b3317518f6
ABDALLAHAHMAD <lyl44271@gmail.com>
2020-10-17 09:11:32 +0300

    created user and cart sagas for sign in, sign up and sign in with google

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -10,10 +10,10 @@ import { ReactComponent as Logo } from '../../assets/4.4 crown.svg.svg';
 
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
-
+import { signOutStart } from '../../redux/user/user.action';
 import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink} from './header.styles';
 
-const Header = ({ currentUser, hidden }) => (
+const Header = ({ currentUser, hidden, signOutStart }) => (
    <HeaderContainer>
       <LogoContainer to='/'>
          <Logo className='logo' />
@@ -27,7 +27,7 @@ const Header = ({ currentUser, hidden }) => (
       </OptionLink>
       {
          currentUser ? (
-         <OptionLink as='div' onClick={() => auth.signOut()}>
+         <OptionLink as='div' onClick={signOutStart}>
             SIGN OUT
          </OptionLink>
          ): (
@@ -45,6 +45,13 @@ const Header = ({ currentUser, hidden }) => (
 const mapStateToProps = createStructuredSelector({
    currentUser: selectCurrentUser,
    hidden: selectCartHidden
- });
+});
 
-export default connect(mapStateToProps)(Header);
+const mapDispatchToProps = dispatch => ({
+   signOutStart: () => dispatch(signOutStart())
+ });
+ 
+ export default connect(
+   mapStateToProps,
+   mapDispatchToProps
+ )(Header);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,66 +12,80 @@ const config = {
    appId: "1:1093100832546:web:b0b51a4b0c4f61e4f4477b",
    measurementId: "G-PBLGYN3VS2"
  }
-export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if(!userAuth) return;
-  const userRef = firestore.doc(`users/${userAuth.uid}`);
-  const snapShot = await userRef.get();
+ firebase.initializeApp(config);
 
-  if(!snapShot.exists) {
-    const { displayName, email } = userAuth;
-    const createAt = new Date();
-
-    try {
-      await userRef.set({
-        displayName,
-        email,
-        createAt,
-        ...additionalData
-      })
-    }catch (error) {
-      console.log('error creating user', error.message);
-    }
-  }
-  return userRef;
-};
-
-export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
-  const collectionRef = firestore.collection(collectionKey);
-  
-  const batch = firestore.batch();
-  objectsToAdd.forEach(obj => {
-    const newDocRef = collectionRef.doc();
-    batch.set(newDocRef, obj);
-  });
-  return await batch.commit();
-};
-
-export const convertCollectionsSnapshotToMap = collections => {
-  const transformedCollection = collections.docs.map(doc => {
-    const {title, items} = doc.data();
-
-    return {
-      routeName: encodeURI(title.toLowerCase()),
-      id: doc.id,
-      title,
-      items
-    };
-  });
-  return transformedCollection.reduce((accmulator, collection) => {
-    accmulator[collection.title.toLowerCase()] = collection;
-    return accmulator;
-  }, {});
-}
-
-
-
-firebase.initializeApp(config);
-
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
-
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ 'prompt' : 'select_account'});
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
-
-export default firebase;
+ export const createUserProfileDocument = async (userAuth, additionalData) => {
+   if (!userAuth) return;
+ 
+   const userRef = firestore.doc(`users/${userAuth.uid}`);
+ 
+   const snapShot = await userRef.get();
+ 
+   if (!snapShot.exists) {
+     const { displayName, email } = userAuth;
+     const createdAt = new Date();
+     try {
+       await userRef.set({
+         displayName,
+         email,
+         createdAt,
+         ...additionalData
+       });
+     } catch (error) {
+       console.log('error creating user', error.message);
+     }
+   }
+ 
+   return userRef;
+ };
+ 
+ export const addCollectionAndDocuments = async (
+   collectionKey,
+   objectsToAdd
+ ) => {
+   const collectionRef = firestore.collection(collectionKey);
+ 
+   const batch = firestore.batch();
+   objectsToAdd.forEach(obj => {
+     const newDocRef = collectionRef.doc();
+     batch.set(newDocRef, obj);
+   });
+ 
+   return await batch.commit();
+ };
+ 
+ export const convertCollectionsSnapshotToMap = collections => {
+   const transformedCollection = collections.docs.map(doc => {
+     const { title, items } = doc.data();
+ 
+     return {
+       routeName: encodeURI(title.toLowerCase()),
+       id: doc.id,
+       title,
+       items
+     };
+   });
+ 
+   return transformedCollection.reduce((accumulator, collection) => {
+     accumulator[collection.title.toLowerCase()] = collection;
+     return accumulator;
+   }, {});
+ };
+ 
+ export const getCurrentUser = () => {
+   return new Promise((resolve, reject) => {
+     const unsubscribe = auth.onAuthStateChanged(userAuth => {
+       unsubscribe();
+       resolve(userAuth);
+     }, reject);
+   });
+ };
+ 
+ export const auth = firebase.auth();
+ export const firestore = firebase.firestore();
+ 
+ export const googleProvider = new firebase.auth.GoogleAuthProvider();
+ googleProvider.setCustomParameters({ prompt: 'select_account' });
+ export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
+ 
+ export default firebase;
